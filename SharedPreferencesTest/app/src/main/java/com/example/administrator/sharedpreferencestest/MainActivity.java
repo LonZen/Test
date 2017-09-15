c265e00e0e030b45683f9cc6c5b917477cd50c6e
Lonzen <529786543@163.com>
2017-09-15 10:48:23 +0800

    2017091510

--- SharedPreferencesTest/app/src/main/java/com/example/administrator/sharedpreferencestest/MainActivity.java
+++ SharedPreferencesTest/app/src/main/java/com/example/administrator/sharedpreferencestest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.sharedpreferencestest;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,8 +20,21 @@ public class MainActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor=getSharedPreferences("data", MODE_PRIVATE).edit();
                 editor.putString("name","Tom");
                 editor.putInt("age",21);
-                editor.putBoolean("married",false);
+                editor.putBoolean("married",true);
                 editor.apply();
+            }
+        });
+        Button restoreData=(Button)findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref=getSharedPreferences("data",MODE_PRIVATE);
+                String name=pref.getString("name","");
+                int age=pref.getInt("age",0);
+                boolean married=pref.getBoolean("married",false);
+                Log.d("MainActivity","name is "+name);
+                Log.d("MainActivity", "age is "+age);
+                Log.d("MainActivity", "married is "+married);
             }
         });
     }
